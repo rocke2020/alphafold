b3a6908724caa5dd78f77017b95465760213554a
qichang.dong <qichang.dong@videt.cn>
2023-02-26 18:19:56 +0800

    u

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,2 @@
-nohup ./download_all_data.sh /home/qcdong/af_data > log.log 2>&1 &
+nohup ./download_uniref90.sh /home/qcdong/af_data > log-uniref90.log 2>&1 &
+# nohup ./download_all_data.sh /home/qcdong/af_data > log.log 2>&1 &
